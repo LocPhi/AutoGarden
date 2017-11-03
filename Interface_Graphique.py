85a281954cf0a09d62bdf833c851859919040ff7
LocPhi <loic.philippot2@gmail.com>
2017-11-03 22:49:20 +0100

    find a way to display different informations in the same windows

--- Interface_Graphique.py
+++ Interface_Graphique.py
@@ -35,16 +35,80 @@
 # interface.mainloop()
 # interface.destroy()
 
+# from tkinter import *
+#
+#
+# def faireApparaitreLeToplevel():
+#     top = Toplevel(root)
+#     lab = Label(top, text="Ce soir je vais manger des frites")
+#     lab.pack()
+#
+#
+# root = Tk()
+# go = Button(root, text="lancer", command=faireApparaitreLeToplevel)
+# go.pack()
+# root.mainloop()
+
+# import tkinter as tk
+#
+# class Demo1:
+#     def __init__(self, master):
+#         self.master = master
+#         self.frame = tk.Frame(self.master)
+#         self.button1 = tk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
+#         self.button1.pack()
+#         self.frame.pack()
+#
+#     def new_window(self):
+#         self.newWindow = tk.Toplevel(self.master)
+#         self.app = Demo2(self.newWindow)
+#
+# class Demo2:
+#     def __init__(self, master):
+#         self.master = master
+#         self.frame = tk.Frame(self.master)
+#         self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+#         self.quitButton.pack()
+#         self.frame.pack()
+#
+#     def close_windows(self):
+#         self.master.destroy()
+#
+# def main():
+#     root = tk.Tk()
+#     app = Demo1(root)
+#     root.mainloop()
+#
+# if __name__ == '__main__':
+#     main()
+
 from tkinter import *
 
 
-def faireApparaitreLeToplevel():
-    top = Toplevel(root)
-    lab = Label(top, text="Ce soir je vais manger des frites")
-    lab.pack()
-
+def raise_frame(frame):
+    frame.tkraise()
 
 root = Tk()
-go = Button(root, text="lancer", command=faireApparaitreLeToplevel)
-go.pack()
+
+f1 = Frame(root)
+f2 = Frame(root)
+f3 = Frame(root)
+f4 = Frame(root)
+
+for frame in (f1, f2, f3, f4):
+    frame.grid(row=0, column=0, sticky='news')
+
+Button(f1, text='Go to frame 2', command=lambda:raise_frame(f2)).pack()
+Label(f1, text='FRAME 1').pack()
+
+Label(f2, text='FRAME 2').pack()
+Button(f2, text='Go to frame 3', command=lambda:raise_frame(f3)).pack()
+
+Label(f3, text='FRAME 3').pack(side='left')
+Button(f3, text='Go to frame 4', command=lambda:raise_frame(f4)).pack(side='left')
+
+Label(f4, text='FRAME 4').pack()
+Button(f4, text='Goto to frame 1', command=lambda:raise_frame(f1)).pack()
+
+raise_frame(f1)
 root.mainloop()
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 You can find here the code for the AutoGarden Graphic Interface
 
+Current progession : fin a way to display different information in the same windows. For exemple, when you click "Chose Plant", it displays the "Choose Plant" menu in the same windows, and not in a difefrent one.
+
 Developped by LocPhi
