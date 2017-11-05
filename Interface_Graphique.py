88635ad7cd6e44dad5f95be2fceb512bb2b32146
LocPhi <loic.philippot2@gmail.com>
2017-11-05 21:55:21 +0100

    Begin the first windows My Plants

--- Interface_Graphique.py
+++ Interface_Graphique.py
@@ -98,8 +98,11 @@ f4 = Frame(root)
 for frame in (f1, f2, f3, f4):
     frame.grid(row=0, column=0, sticky='news')
 
-Button(f1, text='Go to frame 2', command=lambda:raise_frame(f2)).pack()
-Label(f1, text='FRAME 1').pack()
+
+l = LabelFrame(f1, text="My plants", padx=200, pady=150)
+l.pack(fill="both", expand="yes")
+
+Button(l, text='Go to frame 2', command=lambda:raise_frame(f2)).pack()
 
 Label(f2, text='FRAME 2').pack()
 Button(f2, text='Go to frame 3', command=lambda:raise_frame(f3)).pack()
