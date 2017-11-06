5c58d16a974fbf791b3c80abeb5aba45e5712c22
LocPhi <loic.philippot2@gmail.com>
2017-11-06 10:23:56 +0100

    Finish the first frame (CAUTION : ONLY WORK PERFECTLY ON UNIX, BUTTON PROBLEM ON MAC SYSTEM)

--- Interface_Graphique.py
+++ Interface_Graphique.py
@@ -1,113 +1,55 @@
-# from tkinter import *
-#
-#
-# class Interface(Frame):
-#     """Notre fenêtre principale.
-#     Tous les widgets sont stockés comme attributs de cette fenêtre."""
-#
-#     def __init__(self, fenetre, **kwargs):
-#         Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
-#         self.pack(fill=BOTH)
-#         self.nb_clic = 0
-#
-#         # Création de nos widgets
-#         self.message = Label(self, text="Vous n'avez pas cliqué sur le bouton.")
-#         self.message.pack()
-#
-#         self.bouton_quitter = Button(self, text="Quitter", command=self.quit)
-#         self.bouton_quitter.pack(side="left")
-#
-#         self.bouton_cliquer = Button(self, text="Cliquez ici", fg="red",
-#                                      command=self.cliquer)
-#         self.bouton_cliquer.pack(side="right")
-#
-#     def cliquer(self):
-#         """Il y a eu un clic sur le bouton.
-#
-#         On change la valeur du label message."""
-#
-#         self.nb_clic += 1
-#         self.message["text"] = "Vous avez cliqué {} fois.".format(self.nb_clic)
-#
-# fenetre = Tk()
-# interface = Interface(fenetre)
-#
-# interface.mainloop()
-# interface.destroy()
-
-# from tkinter import *
-#
-#
-# def faireApparaitreLeToplevel():
-#     top = Toplevel(root)
-#     lab = Label(top, text="Ce soir je vais manger des frites")
-#     lab.pack()
-#
-#
-# root = Tk()
-# go = Button(root, text="lancer", command=faireApparaitreLeToplevel)
-# go.pack()
-# root.mainloop()
-
-# import tkinter as tk
-#
-# class Demo1:
-#     def __init__(self, master):
-#         self.master = master
-#         self.frame = tk.Frame(self.master)
-#         self.button1 = tk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
-#         self.button1.pack()
-#         self.frame.pack()
-#
-#     def new_window(self):
-#         self.newWindow = tk.Toplevel(self.master)
-#         self.app = Demo2(self.newWindow)
-#
-# class Demo2:
-#     def __init__(self, master):
-#         self.master = master
-#         self.frame = tk.Frame(self.master)
-#         self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
-#         self.quitButton.pack()
-#         self.frame.pack()
-#
-#     def close_windows(self):
-#         self.master.destroy()
-#
-# def main():
-#     root = tk.Tk()
-#     app = Demo1(root)
-#     root.mainloop()
-#
-# if __name__ == '__main__':
-#     main()
+# ------- Grenoble INP - ESISAR -------
+# -- PX504 : AutoGarden ---
+# ---- Author : Loc Phi ----
+# -- Date : 06/10/2017 --
 
 from tkinter import *
 
 def raise_frame(frame):
     frame.tkraise()
 
+# Main Window
 root = Tk()
 
+# All the frame that depend on the main window
 f1 = Frame(root)
 f2 = Frame(root)
 f3 = Frame(root)
 f4 = Frame(root)
+f5 = Frame(root)
 
-for frame in (f1, f2, f3, f4):
+# Place the frame with a grid method
+for frame in (f1, f2, f3, f4, f5):
     frame.grid(row=0, column=0, sticky='news')
 
+# LabelFrame "My Plant" to group all my Button
+myButtons=LabelFrame(f1, text="My Plants", padx=5, pady=5)
 
-l = LabelFrame(f1, text="My plants", padx=200, pady=150)
-l.pack(fill="both", expand="yes")
+# Four buttons to access plants graph
+one = Button(myButtons, text="PLANT 1", command=lambda:raise_frame(f2))
+two = Button(myButtons, text="PLANT 2", command=lambda:raise_frame(f3))
+three = Button(myButtons, text="PLANT 3", command=lambda:raise_frame(f4))
+four = Button(myButtons, text="PLANT 4", command=lambda:raise_frame(f5))
 
-b1=Button(l, text='Go to frame 2', command=lambda:raise_frame(f2))
-b1.pack()
+f1.grid(column=0, row=0, sticky="nsew")
+myButtons.grid(column=0, row=0, sticky="nsew")
 
-b2 = Button(l, text='Go to frame 2', command=lambda:raise_frame(f2)).pack()
-b3 = Button(l, text='Go to frame 2', command=lambda:raise_frame(f2)).pack()
-b4 = Button(l, text='Go to frame 2', command=lambda:raise_frame(f2)).pack()
+one.grid(column=0, row=0, columnspan=1, sticky="nsew")
+two.grid(column=1, row=0, columnspan=1, sticky="nsew")
+three.grid(column=0, row=1, columnspan=1, sticky="nsew")
+four.grid(column=1, row=1, columnspan=1, sticky="nsew")
 
+# Configure the weight to allow the content to expand with the window
+root.grid_rowconfigure(0,weight=1)
+root.grid_columnconfigure(0,weight=1)
+
+f1.grid_rowconfigure(0, weight=1)
+f1.grid_columnconfigure(0, weight=1)
+
+myButtons.grid_rowconfigure(0, weight=1)
+myButtons.grid_rowconfigure(1, weight=1)
+myButtons.grid_columnconfigure(0, weight=1)
+myButtons.grid_columnconfigure(1, weight=1)
 
 Label(f2, text='FRAME 2').pack()
 Button(f2, text='Go to frame 3', command=lambda:raise_frame(f3)).pack()
--- firstFrame.py
+++ firstFrame.py
@@ -2,11 +2,11 @@ from tkinter import *
 
 root = Tk()
 root.title('Example')
-root.geometry('300x300')
+#root.geometry('300x300')
 
 content=Frame(root)
 
-myButtons=LabelFrame(content, text="My Buttons", padx=7, pady=7)
+myButtons=LabelFrame(content, text="My Buttons", padx=5, pady=5)
 
 one = Button(myButtons, text="Button 1")
 two = Button(myButtons, text="Button 2")
@@ -16,10 +16,10 @@ four = Button(myButtons, text="Button 4")
 content.grid(column=0, row=0, sticky="nsew")
 myButtons.grid(column=0, row=0, sticky="nsew")
 
-one.grid(column=0, row=0, sticky="nsew")
-two.grid(column=1, row=0, sticky="nsew")
-three.grid(column=0, row=1, sticky="nsew")
-four.grid(column=1, row=1, sticky="nsew")
+one.grid(column=0, row=0, columnspan=1, sticky="nsew")
+two.grid(column=1, row=0, columnspan=1, sticky="nsew")
+three.grid(column=0, row=1, columnspan=1, sticky="nsew")
+four.grid(column=1, row=1, columnspan=1, sticky="nsew")
 
 root.grid_rowconfigure(0,weight=1)
 root.grid_columnconfigure(0,weight=1)
