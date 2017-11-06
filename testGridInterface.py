7a75d1bd72acdc7b76e73a304a90ec5fc8961d8a
LocPhi <loic.philippot2@gmail.com>
2017-11-06 08:47:48 +0100

    find a way to expand content when the windows size change

--- testGridInterface.py
+++ testGridInterface.py
@@ -2,8 +2,10 @@ from tkinter import *
 
 root = Tk()
 root.title('Example')
+root.geometry('300x300')
 
 content=Frame(root)
+
 myButtons=LabelFrame(content, text="My Buttons", padx=7, pady=7)
 
 one = Button(myButtons, text="Button 1")
@@ -11,17 +13,23 @@ two = Button(myButtons, text="Button 2")
 three = Button(myButtons, text="Button 3")
 four = Button(myButtons, text="Button 4")
 
-content.grid(column=0, row=0)
-myButtons.grid(column=0, row=0)
+content.grid(column=0, row=0, sticky="nsew")
+myButtons.grid(column=0, row=0, sticky="nsew")
 
-one.grid(column=0, row=0)
-two.grid(column=1, row=0)
-three.grid(column=0, row=1)
-four.grid(column=1, row=1)
+one.grid(column=0, row=0, sticky="nsew")
+two.grid(column=1, row=0, sticky="nsew")
+three.grid(column=0, row=1, sticky="nsew")
+four.grid(column=1, row=1, sticky="nsew")
 
-root.grid_columnconfigure(0,weight=1)
-root.grid_columnconfigure(1,weight=1)
 root.grid_rowconfigure(0,weight=1)
-root.grid_rowconfigure(1,weight=1)
+root.grid_columnconfigure(0,weight=1)
+
+content.grid_rowconfigure(0, weight=1)
+content.grid_columnconfigure(0, weight=1)
+
+myButtons.grid_rowconfigure(0, weight=1)
+myButtons.grid_rowconfigure(1, weight=1)
+myButtons.grid_columnconfigure(0, weight=1)
+myButtons.grid_columnconfigure(1, weight=1)
 
 root.mainloop()
